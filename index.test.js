db1743b41091a610630c1e0df4c12d575c87da5d
Shailja Nandan <nandanticm@Shailjas-MacBook-Pro.local>
2024-12-17 14:57:16 +0530

    adding more test cases Number comparison and Array matchers

--- index.test.js
+++ index.test.js
@@ -11,3 +11,32 @@ test("3 and  5 multiply not eqal 10", async()=>{
     expect (multiply(3,5)).not.toBe(10);   //exact equality
 })
 
+test("Object Equality ", ()=>{
+    const data = {alpha :"A"};
+    data ["beta"]= "B";
+    expect(data).toEqual({alpha:"A", beta:"B"}) //deep equality
+})
+
+test ("Null Values ", ()=>{
+    const value = null ;
+    expect(value).toBeNull()
+    expect(value).toBeDefined()
+    expect(value).not.toBeUndefined()
+    expect(value).not.toBeTruthy()
+})
+
+test('NuberComparison', ()=>{
+    const  value = 3+3 ;
+    expect(value).toBeGreaterThan(5);
+    expect(value).toBeGreaterThanOrEqual(5);
+    const floatValue= 0.2 + 0.3;
+    expect(floatValue).toBeCloseTo(0.5, 5);
+
+})
+
+test("Array Matcher", ()=>{
+    const toDoList=[
+        'Running', "Coding", "Eating", "Cooking", "Sleeping"
+    ]
+    expect(toDoList).toContain("Eating")
+})
