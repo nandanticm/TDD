8b57695eb565ed034248bc5f170c58e22c734de9
Shailja Nandan <nandanticm@Shailjas-MacBook-Pro.local>
2024-12-17 12:50:46 +0530

    added not equal test case

--- index.test.js
+++ index.test.js
@@ -3,6 +3,10 @@ const multiply= require("./index")
 //test suite 
 
 
-test("3 and  5 multiply eqal 10", async()=>{
+test("3 and  5 multiply eqal 15", async()=>{
     expect (multiply(3,5)).toBe(15); //exact eqality
+})
+
+test("3 and  5 multiply not eqal 10", async()=>{
+    expect (multiply(3,5)).not.toBe(10);   //exact equality
 })
